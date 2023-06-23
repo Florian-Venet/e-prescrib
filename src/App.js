23b72aaf745a523916ea97b910d4a6112382bd2e
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-23 16:04:14 +0200

    style: réagencement

--- src/App.js
+++ src/App.js
@@ -3,36 +3,36 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="content-container">
-        <header className="App-header">
-        
+      <div className="App">
+        <div className="content-container">
+          <header className="App-header">
             <div className="logo-container">
               <img src="./logo.png" alt="logo" className="logo" />
             </div>
-        </header>
+            <div className="header-divider"></div>
+          </header>
             <div className="left-section">
-            <div className="login-section">
-              <p><h2>Connexion</h2></p>
-              <div className="input-container">
-                <label htmlFor="email">E-mail</label>
-                <br />
-                <input type="text" id="email" />
+              <div className="login-section">
+                <p><h2>Connexion</h2></p>
+                <div className="input-container">
+                  <label htmlFor="email">E-mail</label>
+                  <br />
+                  <input type="text" id="email" />
+                </div>
+                <div className="input-container">
+                  <label htmlFor="password">Mot de passe</label>
+                  <br />
+                  <input type="password" id="password" />
+                </div>
+                <p>Pas encore inscrit ? <a href="/inscription">Inscription</a></p>
               </div>
-              <div className="input-container">
-                <label htmlFor="password">Mot de passe</label>
-                <br />
-                <input type="password" id="password" />
-              </div>
-              <p>Pas encore inscrit ? <a href="/inscription">Inscription</a></p>
+            </div>
+            <div className="vertical-line"></div>
+            <div className="right-section">
+              {/* Contenu de la partie droite */}
             </div>
           </div>
-          <div className="vertical-line"></div>
-          <div className="right-section">
-            {/* Contenu de la partie droite */}
-          </div>
-        </div>
-    </div>
+      </div>
   );
 }
 
