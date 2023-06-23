9837bc3cd0cc6b44e10b16b97465aadbec1702b1
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-23 14:54:31 +0200

    construction générale de la page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left-section">
+          <img src="chemin/vers/votre/logo.png" alt="logo" className="logo" />
+          <div className="login-section">
+            <p>Connexion</p>
+            <div className="input-container">
+              <label htmlFor="email">E-mail</label>
+              <input type="text" id="email" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="password">Mot de passe</label>
+              <input type="password" id="password" />
+            </div>
+            <p>Pas encore inscrit ? <a href="/inscription">Inscription</a></p>
+          </div>
+        </div>
+        <div className="vertical-line"></div>
+        <div className="right-section">
+          {/* Contenu de la partie droite */}
+        </div>
       </header>
     </div>
   );
