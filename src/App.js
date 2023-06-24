150eb709a969b54f691217c5bb5fabd16a23b17c
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-24 23:37:13 +0200

    docs: ajout documentation right part

--- src/App.js
+++ src/App.js
@@ -3,36 +3,43 @@ import './App.css';
 
 function App() {
   return (
-      <div className="App">
-        <div className="content-container">
-          <header className="App-header">
-            <div className="logo-container">
-              <img src="./logo.png" alt="logo" className="logo" />
-            </div>
-            <div className="header-divider"></div>
-          </header>
-            <div className="left-section">
-              <div className="login-section">
-                <p><h2>Connexion</h2></p>
-                <div className="input-container">
-                  <label htmlFor="email">E-mail</label>
-                  <br />
-                  <input type="text" id="email" />
-                </div>
-                <div className="input-container">
-                  <label htmlFor="password">Mot de passe</label>
-                  <br />
-                  <input type="password" id="password" />
-                </div>
-                <p>Pas encore inscrit ? <a href="/inscription">Inscription</a></p>
-              </div>
-            </div>
-            <div className="vertical-line"></div>
-            <div className="right-section">
-              {/* Contenu de la partie droite */}
-            </div>
+    <div className="App">
+      <div className="content-container">
+        <header className="App-header">
+          <div className="logo-container">
+            <img src="./logo.png" alt="logo" className="logo" />
           </div>
+          <div className="header-divider"></div>
+        </header>
+        <div className="left-section">
+          <div className="login-section">
+            <h2>Connexion</h2>
+            <div className="input-container">
+              <label htmlFor="email">E-mail</label>
+              <br />
+              <input type="text" id="email" />
+            </div>
+            <div className="input-container">
+              <label htmlFor="password">Mot de passe</label>
+              <br />
+              <input type="password" id="password" />
+            </div>
+            <p>Pas encore inscrit ? <a href="/inscription">Inscription</a></p>
+          </div>
+        </div>
+        <div className="vertical-line"></div>
+        <div className="right-section">
+          <div className="right-content">
+            <h2>Pourquoi utiliser ePrescrib ?</h2>
+            <p>ePrescrip simplifie grandement les rapports entre médecins, patients et pharmaciens. Grâce à notre ordonnance digitalisée, fini l'utilisation intensive de papier.</p>
+            <p>Un geste pour la planète, et un geste pour le pharmacien qui pourra enfin lire votre ordonnance sans appeler des collègues pour vous déchiffrer.</p>
+            <h2>Comment ça fonctionne ?</h2>
+            <p>Appuez sur "nouvelle ordonnance pour créer un nouveau document, renseignez les informations nécessaire et finaliser là en appuyant sur "générer", ce qui va créer le QR code pour autentifier l'ordonnance. Le patient n'a plus qu'a se rendre sur son application ePrescrib et scanner le QR code avant de se rendre en pharamacie </p>
+            <img src="./ordonnance.jpg" alt="image" className="custom-image" />
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
 
