f88a8cfb7ce0ed469addf837d9f692facbe6dbef
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-25 17:09:08 +0200

    style: note ensemble

--- src/App.js
+++ src/App.js
@@ -6,71 +6,62 @@ import Main from "./Main";
 import Sidebar from "./Sidebar";
 
 function Note() {
-  <header className="App-header">
-    <div className="logo-container">
-      <div className="header-text">
-        <h2>ePrescrin</h2>
-      </div>
-      <img src="./logo.png" alt="logo" className="logo" />
+  const [notes, setNotes] = useState(
+    localStorage.notes ? JSON.parse(localStorage.notes) : []
+  );
+  const [activeNote, setActiveNote] = useState(false);
+  
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+  
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "Untitled Note",
+      body: "",
+      lastModified: Date.now(),
+    };
+  
+    setNotes([newNote, ...notes]);
+    setActiveNote(newNote.id);
+  };
+  
+  const onDeleteNote = (noteId) => {
+    setNotes(notes.filter(({ id }) => id !== noteId));
+  };
+  
+  const onUpdateNote = (updatedNote) => {
+    const updatedNotesArr = notes.map((note) => {
+      if (note.id === updatedNote.id) {
+        return updatedNote;
+      }
+  
+      return note;
+    });
+  
+    setNotes(updatedNotesArr);
+  };
+  
+  const getActiveNote = () => {
+    return notes.find(({ id }) => id === activeNote);
+  };
+  
+  return (
+    <div className="note">
+      <Sidebar
+        notes={notes}
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
     </div>
-    <div className="header-divider"></div>
-  </header>
-    const [notes, setNotes] = useState(
-      localStorage.notes ? JSON.parse(localStorage.notes) : []
-    );
-    const [activeNote, setActiveNote] = useState(false);
+  );
+}
   
-    useEffect(() => {
-      localStorage.setItem("notes", JSON.stringify(notes));
-    }, [notes]);
-  
-    const onAddNote = () => {
-      const newNote = {
-        id: uuid(),
-        title: "Untitled Note",
-        body: "",
-        lastModified: Date.now(),
-      };
-  
-      setNotes([newNote, ...notes]);
-      setActiveNote(newNote.id);
-    };
-  
-    const onDeleteNote = (noteId) => {
-      setNotes(notes.filter(({ id }) => id !== noteId));
-    };
-  
-    const onUpdateNote = (updatedNote) => {
-      const updatedNotesArr = notes.map((note) => {
-        if (note.id === updatedNote.id) {
-          return updatedNote;
-        }
-  
-        return note;
-      });
-  
-      setNotes(updatedNotesArr);
-    };
-  
-    const getActiveNote = () => {
-      return notes.find(({ id }) => id === activeNote);
-    };
-  
-    return (
-      <div className="note">
-        <Sidebar
-          notes={notes}
-          onAddNote={onAddNote}
-          onDeleteNote={onDeleteNote}
-          activeNote={activeNote}
-          setActiveNote={setActiveNote}
-        />
-        <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote} />
-      </div>
-    );
-  }
-  
-  export default Note;
+export default Note;
 
 
 
