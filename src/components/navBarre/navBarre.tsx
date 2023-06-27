4077b4d00da294ac3e8d8e5e1669f88434a604c6
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-27 10:57:41 +0200

    feat: navBarre

---
+++ src/components/navBarre/navBarre.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export default function NavBarre(){
+    return(
+        <div className={styles.navBarre}>
+            djso
+        
+        </div>
+        
+    )
+}
