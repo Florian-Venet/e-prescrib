02e087b59430fd5dbf9ec2774ab7ef888eeea140
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-27 21:12:55 +0200

    feat: ordi list

--- src/components/navBarre/navBarre.tsx
+++ src/components/navBarre/navBarre.tsx
@@ -1,11 +1,18 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
 import styles from './styles.module.css';
+import { LogoutOutlined } from '@mui/icons-material/';
 
-export default function NavBarre(){
-    return(
-        <div className={styles.navBarre}>
-            djso
-        
-        </div>
-        
-    )
+export default function NavBarre() {
+  return (
+    <div className={styles.navBarre}>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <img src="./src/assets/logo.png" style={{ marginRight: '10px' }} />
+          <Typography sx={{ fontFamily: 'Montserrat' }} variant="h5">ePrescrib</Typography>
+        </Box>
+        <LogoutOutlined sx={{ color: '#c94d3a' }} />
+      </Box>
+    </div>
+  );
 }
