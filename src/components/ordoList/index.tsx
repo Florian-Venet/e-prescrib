a1de8d85647cf21473de6de1dcec2d7a1d7a279e
444omaru <omarmmt8@gmail.com>
2023-06-30 12:05:37 +0200

    (feat):add button pour ordonnance

--- src/components/ordoList/index.tsx
+++ src/components/ordoList/index.tsx
@@ -12,64 +12,12 @@ import Divider from '@mui/material/Divider';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
 
-function renderRow(props: { index: any; style: any; }) {
-    const { index, style } = props;
-  
-    return (
-      <ListItem style={style} key={index} component="div" disablePadding>
-        <ListItemButton>
-          <ListItemText primary={`Item ${index + 1}`} />
-        </ListItemButton>
-      </ListItem>
-    );
-  }
+
 export default function App() {
   return (
-    
-    <div className={styles.title}>
-        <Typography sx={{fontFamily: "Montserrat"}} variant='h5'>
-          Mes ordonnances
-        </Typography>
-        <Box sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}  />
-        
-      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <nav aria-label="main mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-      <Divider />
-      <nav aria-label="secondary mailbox folders">
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemText primary="Trash" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton component="a" href="#simple-list">
-              <ListItemText primary="Spam" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </nav>
-    </Box>
-    </div>
+    <Fab color="primary" aria-label="add">
+  <AddIcon />
+</Fab>
   );
 
 }
