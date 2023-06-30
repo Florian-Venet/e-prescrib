1f6b2a687232606fa0fdce0a545539e02c090ce3
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-30 20:35:59 +0200

    feat: cases médicament s

--- src/components/newOrdo/index.tsx
+++ src/components/newOrdo/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Typography, Box, IconButton, Button, Divider, TextField } from '@mui/material';
+import { Typography, Box, IconButton, Button, Divider, TextField, Grid } from '@mui/material';
 import styles from './styles.module.css';
 import LogoutIcon from '@mui/icons-material/Logout';
 import { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 import useIsMobile from '../../hooks/useIsMobile';
+import MenuItem from "@mui/material/MenuItem";
 
+const médicaments = [
+  { value: "aucun", label: "Aucun" },
+  { value: "paracétamol", label: "Paracétamol" },
+  { value: "ibuprofène", label: "Ibuprofène" },
+  { value: "tramadol ", label: "Tramadol " },
+  { value: "amoxicilline", label: "Amoxicilline" },
+  { value: "cholécalciférol", label: "Cholécalciférol" },
+  { value: "phloroglucinol", label: "Phloroglucinol" },
+  { value: "martinmeniole", label: "Martinmeniole" }
+];
+
+const boites = [
+  { value: "0", label: "0" },
+  { value: "1", label: "1" },
+  { value: "2", label: "2" },
+  { value: "3", label: "3" },
+  { value: "4", label: "4" },
+  { value: "5", label: "5" }
+];
 
 export default function NewOrdo(){
   const isMobile = useIsMobile();
@@ -13,10 +33,15 @@ export default function NewOrdo(){
   return (
     <div className={styles.content} style={{width: !isMobile ? "30vw" : "80vw"}}>
         <div className={styles.title}>
-          <Typography sx={{fontFamily: "Montserrat"}} variant='h5'>
+          <Typography sx={{fontFamily: "Montserrat", m: 2}} variant='h5'>
             Nouvelle ordonnance
           </Typography>
         </div>  
+
+    
+
+
+
 
         <Box
           component="form"
@@ -29,6 +54,59 @@ export default function NewOrdo(){
           <TextField id="outlined-basic" label="Prénom" variant="outlined" />
           <TextField id="outlined-basic" label="Nom" variant="outlined" />
         </Box>
+        
+        
+        
+        
+        
+
+        <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 2, width: '25ch' },
+      }}
+      noValidate
+    >
+      <Grid container spacing={9}>
+        <Grid item xs={6}>
+          <TextField
+            id="outlined-select-médicaments"
+            select
+            label="Médicaments"
+            defaultValue="aucun"
+            variant="outlined"
+          >
+            {médicaments.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            id="outlined-select-boites-native"
+            select
+            label="Nombres de boîtes"
+            defaultValue="0"
+            SelectProps={{
+              native: true,
+            }}
+            variant="outlined"
+          >
+            {boites.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
+    </Box>
+        
+
+
+
 
 
         <Box
@@ -43,14 +121,17 @@ export default function NewOrdo(){
             <TextField
               id="outlined-multiline-static"
              multiline
-              rows={8}
+              rows={7}
               defaultValue="Commentaire"
             />
           </div>
         </Box>
 
 
-        <Button variant='contained' sx={{backgroundColor: "#4153EF", borderRadius: "15px"}} disableElevation>
+
+
+
+        <Button variant='contained' sx={{backgroundColor: "#4153EF", borderRadius: "15px", m: 2}} disableElevation>
             Créer l'ordonnance
         </Button>
 
