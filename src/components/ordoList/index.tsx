3d8a3cb993136809327818e87e5cbe4b00c92ad3
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-06-30 11:06:27 +0200

    feat: truc

--- src/components/ordoList/index.tsx
+++ src/components/ordoList/index.tsx
@@ -3,10 +3,14 @@ import { Typography, Box, Fab } from '@mui/material';
 import styles from './styles.module.css';
 import { LogoutOutlined } from '@mui/icons-material/';
 import AddIcon from '@mui/icons-material/Add';
-import { FixedSizeList } from 'react-window';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
 function renderRow(props: { index: any; style: any; }) {
     const { index, style } = props;
@@ -26,25 +30,46 @@ export default function App() {
         <Typography sx={{fontFamily: "Montserrat"}} variant='h5'>
           Mes ordonnances
         </Typography>
-        <Box
-      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
-    >
-      <FixedSizeList
-        height={400}
-        width={360}
-        itemSize={46}
-        itemCount={20}
-        overscanCount={5}
-      >
-        {renderRow}
-      </FixedSizeList>
+        <Box sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}  />
+        
+      <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+      <nav aria-label="secondary mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Trash" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="#simple-list">
+              <ListItemText primary="Spam" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
     </Box>
-        <Box sx={{ '& > :not(style)': { m: 1 } }}>
-        <Fab size="small" color="secondary" aria-label="add">
-            <AddIcon />
-        </Fab>
-        </Box>
-
     </div>
   );
+
 }
