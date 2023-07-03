090a81555b583cacc3209a969eaa328c4f7b2638
lorematteo <loremattteo@gmail.com>
2023-07-03 15:29:31 +0200

    feat: home button added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,15 @@
 import { Add } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import NavBar from "../components/navBar/navBar";
 import OrdoList from "../components/ordoList";
 
 export default function Home(){
+  const navigate = useNavigate();
+
+  const goNew = () => {
+    navigate('/new');
+  }
   return (
     <div style={{display: "flex", flexDirection: "column", justifyContent: "center", gap: "4rem"}}>
       <NavBar/>
@@ -15,7 +21,7 @@ export default function Home(){
         width: "3.5rem",
         height: "3.5rem",
         backgroundColor: "#4153EF"
-      }}>
+      }} onClick={goNew}>
         <Add sx={{color: "white", width: "2rem", height: "2rem"}}/>
       </IconButton>
     </div>
