f924c19af95a8b1127217811bd6120c0bd23c7a2
444omaru <omarmmt8@gmail.com>
2023-07-03 12:24:23 +0200

    (feat):liste

--- src/components/ordoList/index.tsx
+++ src/components/ordoList/index.tsx
@@ -1,16 +1,82 @@
 import React from 'react';
-import { Typography, Box, Fab } from '@mui/material';
+import { Typography, Box, Fab, List, ListItem, ListItemButton, ListItemText, Divider } from '@mui/material';
 import styles from './styles.module.css';
 import AddIcon from '@mui/icons-material/Add';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
 
 
 
 export default function App() {
-  return (
-    <Fab color="primary" aria-label="add" className={styles.addButton} onClick={event => window.location.href='/new'}
-    >
-  <AddIcon />
-</Fab>
-  );
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  return (
+    <>
+    <Fab color="primary" aria-label="add" className={styles.addButton} onClick={event => window.location.href = '/new'}
+    >
+      <AddIcon />
+    </Fab>
+    
+    <Box sx={{ width: '100%', maxWidth: "auto", bgcolor: 'background.paper' }}>
+        <nav aria-label="main mailbox folders">
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton>
+                <ListItemText primary="Ordonnance Doliprane" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton onClick={handleClickOpen}>
+                <ListItemText primary="Ordonnance Ibuprofène" />
+              </ListItemButton>
+            </ListItem>
+            <Divider />
+            <ListItem disablePadding>
+              <ListItemButton>  
+                <ListItemText primary="Ordonnance 3" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Subscribe</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To subscribe to this website, please enter your email address here. We
+            will send updates occasionally.
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Subscribe</Button>
+        </DialogActions>
+      </Dialog>
+        </nav>
+      </Box>    
+      </>
+  );
 }
