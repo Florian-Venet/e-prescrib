4a23bc76a66414f467dacfbd484a59351589c887
444omaru <omarmmt8@gmail.com>
2023-07-03 10:20:01 +0200

    (feat):add button sur la page home pour emmener vers /new

--- src/components/ordoList/index.tsx
+++ src/components/ordoList/index.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Typography, Box, Fab } from '@mui/material';
 import styles from './styles.module.css';
-import { LogoutOutlined } from '@mui/icons-material/';
 import AddIcon from '@mui/icons-material/Add';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import List from '@mui/material/List';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import InboxIcon from '@mui/icons-material/Inbox';
-import DraftsIcon from '@mui/icons-material/Drafts';
+
 
 
 export default function App() {
   return (
-    <Fab color="primary" aria-label="add">
+    <Fab color="primary" aria-label="add" className={styles.addButton} onClick={event => window.location.href='/new'}
+    >
   <AddIcon />
 </Fab>
   );
