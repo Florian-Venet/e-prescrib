bc66ad150c1e6e3a4191bb9850b6555ff003fede
lorematteo <loremattteo@gmail.com>
2023-07-03 15:27:24 +0200

    feat: home updated

--- src/components/ordoList/index.tsx
+++ src/components/ordoList/index.tsx
@@ -1,82 +1,30 @@
-import React from 'react';
-import { Typography, Box, Fab, List, ListItem, ListItemButton, ListItemText, Divider } from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText, Typography } from '@mui/material';
+
 import styles from './styles.module.css';
-import AddIcon from '@mui/icons-material/Add';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
 
 
-
-export default function App() {
-
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
+export default function OrdoList() {
+  const models = ["Mon modèle 1", "Mon modèle 2", "Mon modèle 3"];
 
   return (
-    <>
-    <Fab color="primary" aria-label="add" className={styles.addButton} onClick={event => window.location.href = '/new'}
-    >
-      <AddIcon />
-    </Fab>
-    
-    <Box sx={{ width: '100%', maxWidth: "auto", bgcolor: 'background.paper' }}>
-        <nav aria-label="main mailbox folders">
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary="Ordonnance Doliprane" />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton onClick={handleClickOpen}>
-                <ListItemText primary="Ordonnance Ibuprofène" />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>  
-                <ListItemText primary="Ordonnance 3" />
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions>
-      </Dialog>
-        </nav>
-      </Box>    
-      </>
+    <div className={styles.container}>
+      <Typography variant='h4'>Mes modèles :</Typography>
+      <List>
+        {models.map((item, index) => (
+          <OrdoRaw med={item} key={index}/>
+        ))}
+      </List>
+    </div>
   );
+}
+
+
+function OrdoRaw({med} : {med:string}) {
+  return (
+    <ListItem disablePadding>
+      <ListItemButton>
+        <ListItemText primary={med} />
+      </ListItemButton>
+    </ListItem>
+  )
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,11 +1,23 @@
+import { Add } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import NavBar from "../components/navBar/navBar";
 import OrdoList from "../components/ordoList";
 
 export default function Home(){
   return (
-    <div>
+    <div style={{display: "flex", flexDirection: "column", justifyContent: "center", gap: "4rem"}}>
       <NavBar/>
       <OrdoList/>
+      <IconButton style={{
+        position: "absolute",
+        bottom: "3rem",
+        right: "3rem",
+        width: "3.5rem",
+        height: "3.5rem",
+        backgroundColor: "#4153EF"
+      }}>
+        <Add sx={{color: "white", width: "2rem", height: "2rem"}}/>
+      </IconButton>
     </div>
   )
 }
