5a348012dd999b195c32333c39a7699fe0a88788
Florian-Venet <132278794+Florian-Venet@users.noreply.github.com>
2023-07-04 12:29:02 +0200

    style: carousel ok

--- src/components/infoPanel/carousel/index.tsx
+++ src/components/infoPanel/carousel/index.tsx
@@ -1,5 +1,77 @@
-export default function Carousel(){
+import styles from './styles.module.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import medecin from '../../../assets/medecin.png';
+import pharmacie from '../../../assets/parmacie.png';
+import pharmacie2 from '../../../assets/parmacie2.png';
+import logo from '../../../assets/logo.png';
+
+const steps = [
+  {
+    label: 'Le médecin génère le QR Code',
+    description: <img src={medecin} className={styles.img} />,
+  },
+  {
+    label: 'Le patient le scanne et récupère les informations',
+    description: <img src={pharmacie} className={styles.img} />,
+  },
+  {
+    label: 'La pharmacienne les récupère à son tour',
+    description: <img src={pharmacie2} className={styles.img} />,
+  },
+];
+
+export default function Carousel() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = steps.length;
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
   return (
-    <div>Carousel</div>
-  )
+    <Box sx={{ maxWidth: 750, flexGrow: 1 }}>
+      <div className={styles.carousel}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <div>
+          <Typography variant="h5">ePrescrib</Typography>
+          <Typography variant="subtitle1">Votre santé dans votre poche !</Typography>
+        </div>
+      </div>
+
+      <Box sx={{ height: 456, maxWidth: 300, width: '100%', p: 2 }}>
+        {steps[activeStep].description}
+      </Box>
+      <MobileStepper
+        variant="dots"
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+          </Button>
+        }
+      />
+    </Box>
+  );
 }
+
+
--- src/components/infoPanel/index.tsx
+++ src/components/infoPanel/index.tsx
@@ -1,94 +1,11 @@
+import Carousel from './carousel';
 import styles from './styles.module.css';
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
-import MobileStepper from '@mui/material/MobileStepper';
-import Paper from '@mui/material/Paper';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import medecin from '../../assets/medecin.png';
-import pharmacie from '../../assets/parmacie.png';
-import pharmacie2 from '../../assets/parmacie2.png';
-
-const steps = [
-  {
-    label: 'Le médecin génère le QR Code',
-    description: <img src={medecin} />,
-  },
-  {
-    label: 'Le patient le scanne et récupère les informations',
-    description: <img src={pharmacie} />,
-  },
-  {
-    label: 'La pharmacienne les récupère à son tour',
-    description: <img src={pharmacie2} />,
-  },
-];
 
 
-export default function TextMobileStepper() {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = steps.length;
-
-  const handleNext = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
+export default function Panel(){
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
-      <Paper
-        square
-        elevation={0}
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          height: 40,
-          pl: 2,
-          bgcolor: 'background.default',
-        }}
-      >
-        <Typography>{steps[activeStep].label}</Typography>
-      </Paper>
-      <Box sx={{ height: 255, maxWidth: 400, width: '100%', p: 2 }}>
-        {steps[activeStep].description}
-      </Box>
-      <MobileStepper
-        variant="text"
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-            Back
-          </Button>
-        }
-      />
-    </Box>
-  );
+    <div className={styles.panel}>
+      <Carousel/>
+    </div>
+  )
 }
